b7e66587c5e26223b4a657323c71a0604bcca252
Jared Bagnall <j.bagnall@connect.qut.edu.au>
2016-08-31 22:08:42 +1000

    UserViewModel
    created new view model for user details

---
+++ Application.Core/ViewModels/UserViewModel.cs
@@ -0,0 +1,47 @@
+﻿using MvvmCross.Core.ViewModels;
+using Application.Core.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Application.Core.ViewModels
+{
+	public class UserViewModel : MvxViewModel
+	{
+
+		private string _userName = "Jared";
+
+		public string UserName {
+			get { return _userName; }
+			set { _userName = value;}
+		}
+
+		private string _email = "jaredsbagnall@gmail.com";
+
+		public string Email
+		{
+			get { return _email; }
+			set { _email = value; }
+		}
+
+		private string _password = "password";
+
+		public string Password
+		{
+			get { return _password; }
+			set { _password = value; }
+		}
+
+		private double _startTime;
+		private double _finishTime;
+
+		// set multiple fields with one button??
+
+		public double SetOfficeHours {
+
+			get; set;
+		}
+	}
+}
+
