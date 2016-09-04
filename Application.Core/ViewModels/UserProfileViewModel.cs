adec52df6cf00cde574b5a8fafe52c0e0624ab6c
Jared Bagnall <j.bagnall@connect.qut.edu.au>
2016-09-04 21:10:01 +1000

    Update Authoring Details

--- Application.Core/ViewModels/UserProfileViewModel.cs
+++ Application.Core/ViewModels/UserProfileViewModel.cs
@@ -1,10 +1,51 @@
-﻿using System;
-namespace Application.Core
+﻿using MvvmCross.Core.ViewModels;
+using Application.Core.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+/// <summary>
+/// Author: Jared Bagnall
+/// Student Number: n5686245
+/// Responsible Screen: User Profile Screen
+/// Responsible Files: UserProfileViewModel.cs, UserProfile.axml, UserProfileView.cs
+/// </summary>
+namespace Application.Core.ViewModels
 {
-	public class UserProfileViewModel
+	public class UserProfileViewModel : MvxViewModel
 	{
-		public UserProfileViewModel()
+
+		private string _userName = "Jared";
+
+		public string UserName {
+			get { return _userName; }
+			set {  SetProperty(ref _userName, value); }
+		}
+
+		private string _email = "jaredsbagnall@gmail.com";
+
+		public string Email
 		{
+			get { return _email; }
+			set { SetProperty(ref _email, value); }
+		}
+
+		private string _password = "password";
+
+		public string Password
+		{
+			get { return _password; }
+			set { SetProperty(ref _password, value); }
+		}
+
+		//private double _startTime;
+		//private double _finishTime;
+
+		// set multiple fields with one button??
+
+		public double SetOfficeHours {
+
+			get; set;
 		}
 	}
 }
