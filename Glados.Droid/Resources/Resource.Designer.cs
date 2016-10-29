2fe3b014e74c5a625ef80df113f660bdee72ce47
Sathya Amarsee <sathya.amarsee@connect.qut.edu.au>
2016-10-29 17:35:30 +1000

    Updated visuals to look more professional

--- Glados.Droid/Resources/Resource.Designer.cs
+++ Glados.Droid/Resources/Resource.Designer.cs
@@ -93,25 +93,43 @@ namespace Glados.Droid
 			public const int Icon = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int message_icon = 2130837506;
+			public const int message_icon2 = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int profile_icon = 2130837507;
+			public const int message_icon_contactspage = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int settings_icon = 2130837508;
+			public const int message_icon_messagespage = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int splash = 2130837509;
+			public const int profile_icon = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int star_gold_icon = 2130837510;
+			public const int profile_icon2 = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int star_grey_icon = 2130837511;
+			public const int search_icon = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int user_icon = 2130837512;
+			public const int SearchBlue = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int SearchOrange = 2130837513;
+			
+			// aapt resource value: 0x7f02000a
+			public const int settings_icon = 2130837514;
+			
+			// aapt resource value: 0x7f02000b
+			public const int splash = 2130837515;
+			
+			// aapt resource value: 0x7f02000c
+			public const int star_gold_icon = 2130837516;
+			
+			// aapt resource value: 0x7f02000d
+			public const int star_grey_icon = 2130837517;
+			
+			// aapt resource value: 0x7f02000e
+			public const int user_icon = 2130837518;
 			
 			static Drawable()
 			{
