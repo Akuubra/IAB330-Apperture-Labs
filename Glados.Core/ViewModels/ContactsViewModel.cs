ab0014598e042ea2b23dc8ca2f084c1c354e64f8
ferar <ferar@SILVERCROW>
2016-11-01 18:31:42 +1000

    Snappier contact search (still crashes from spam)

--- Glados.Core/ViewModels/ContactsViewModel.cs
+++ Glados.Core/ViewModels/ContactsViewModel.cs
@@ -172,7 +172,7 @@ namespace Glados.Core.ViewModels
 
         public async void  GetContacts(bool getDatabase)
         {
-            
+            //bool doesExist = false;
             if(getDatabase)
             {
                 _contacts = await database.GetUsers(); /// need to add in wrapping for favourites and also separate details for groups
@@ -195,8 +195,12 @@ namespace Glados.Core.ViewModels
             {
                 bool tempUserFav = false;
                 var User = new ContactWrapper(new Contact(user, tempUserFav), this);
+                /*if (getDatabase)
+                {
+                    doesExist = await fav.favouriteExists(loggedInUser.Id, user.Id);
+                }*/
                 bool doesExist = await fav.favouriteExists(loggedInUser.Id, user.Id);
-                if(doesExist)
+                if (doesExist)
                 {
                     var favTemp = await fav.GetFavourite(loggedInUser.Id, user.Id);
                     tempUserFav = favTemp.isFavourite;
@@ -207,7 +211,7 @@ namespace Glados.Core.ViewModels
                         Debug.WriteLine("Added user"+user.Id+"to list");
                         _favourites.Add(user.Id);
                     }
-                }else
+                }else if(!doesExist && getDatabase)
                 {
                     await fav.InsertFavourite(loggedInUser.Id, user.Id, false);
 
